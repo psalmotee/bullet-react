e3bc20a73b2c75eae3c1483b03e94b8f280f4e4a
Moradeyo Samson Tolulope <samsonmoradeyo@gmail.com>
2025-06-27 04:37:23 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -106,7 +106,7 @@ function App() {
       <RouterProvider router={router} />
       <ToastContainer
         position="top-right"
-        autoClose={3000}
+        autoClose={1000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
