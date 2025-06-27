ecccc76a6abb31402b29d5daa0afbb89b7b02376
Moradeyo Samson Tolulope <samsonmoradeyo@gmail.com>
2025-06-27 04:40:24 +0100

    Updated DashboardLayout.jsx

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -17,12 +17,12 @@ const DashboardLayout = () => {
   return (
     <div className="flex h-screen bg-gray-50">
       <Sidebar isOpen={isSidebarOpen} onClose={handleSidebarClose} />
-      
-      <div className="flex-1 flex flex-col overflow-hidden lg:ml-0">
+
+      <div className="flex-1 flex flex-col overflow-hidden sm:ml-0 sm:py-4 sm:gap-4">
         <Topbar onMenuClick={handleMenuClick} />
-        
-        <main className="flex-1 overflow-y-auto">
-          <div className="p-4 lg:p-6">
+
+        <main className="flex-1 overflow-y-auto p-4 sm:px-6 sm:py-0 md:gap-8">
+          <div className="py-6">
             <Outlet />
           </div>
         </main>
